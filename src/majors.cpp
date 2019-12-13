8654b65241ae6441526a307efb0d532a6de3c215
Naaafis <52777302+Naaafis@users.noreply.github.com>
2019-12-13 17:19:54 -0500

    Add files via upload
    Hard Coded Eng Majors

--- src/majors.cpp
+++ src/majors.cpp
@@ -94,16 +94,48 @@ int main()
     /* Initialize empty list */
     //LinkedList *list2 = new LinkedList();
 
+
+    //Biomedical Engineering
+
     list->insertEnd("Biomedical Engineering"); 
   
-    list->insertEnd("Required, MA123, 4, ENG EK100, 0, CAS CH101, 4, ENG EK125, 4, CAS WR120, 4, CAS MA124, 4, CAS PY211, 4, CAS CH102, 4, ENG EK131, 2, ENG EK103, 3, CAS MA225, 4, CAS PY212, 4, ENG EK307, 4, ENG EK210, 2, CAS WR150, 4, CAS MA226, 4, ENG BE209, 4, ENG EK301, 4, ENG EK381, 4, CAS BI315, 4, ENG BE403, 4, ENG BE403, 4, ENG BE491, 2, ENG EK424, 4, ENG BE 492, 2, ENG BE465, 2, ENG BE466, 4"); 
+    list->insertEnd("Required, CAS MA123, 4, ENG EK100, 0, CAS CH101, 4, ENG EK125, 4, CAS WR120, 4, CAS MA124, 4, CAS PY211, 4, CAS CH102, 4, ENG EK131, 2, ENG EK103, 3, CAS MA225, 4, CAS PY212, 4, ENG EK307, 4, ENG EK210, 2, CAS WR150, 4, CAS MA226, 4, ENG BE209, 4, ENG EK301, 4, ENG EK381, 4, CAS BI315, 4, ENG BE403, 4, ENG BE403, 4, ENG BE491, 2, ENG EK424, 4, ENG BE 492, 2, ENG BE465, 2, ENG BE466, 4"); 
 
     list->insertEnd("Electives, One Continua & Fields Elective, Two Suitable Professional Electives, One Engineering elective, Two Biomedical engineering electives, One Biomedical engineering design elective");
 
     list->insertEnd("Hub Credits, PIL, AE, HC, SI, IC, GCI, GCI, ER");
 
+    //Computer Engineering
+    list->insertEnd("Computer Engineering");
 
-   
+    list->insertEnd("Required, CAS MA123, 4, CAS CH 131, 4, ENG EK100, 0, ENG EK125, 4, CAS WR120, 4, CAS MA124, 4, CAS PY211, 4, ENG EK131, 2, ENG EK103, 3, CAS WR150, 4, CAS MA225, 4, CAS PY212, 4, ENG EK307, 4, ENG EK327, 4, CAS MA226, 4, ENG EC311, 4, ENG EK301, 4, ENG EK210, 2, ENG EC330, 4, ENG EK381, 4, ENG EC413, 4, CAS MA193, 2, ENG EC463, 4, ENG EC464, 4");
+
+    list->insertEnd("Electives, Two Core Electives, Two Computer Engineering Electives, One EE Breadth Elective, 3 Technical Electives");
+
+    list->insertEnd("Hub Credits, PIL, AE, HC, SI, IC, GCI, GCI, ER");
+
+    //Mechaincal Engineering
+
+    list->insertEnd("Mechanical Engineering");
+
+    list->insertEnd("Required, CAS MA123, 4, CAS CH 131, 4, ENG EK100, 0, ENG EK125, 4, CAS WR120, 4, CAS MA124, 4, CAS PY211, 4, ENG EK131, 2, ENG EK103, 3, CAS WR150, 4, CAS MA225, 4, CAS PY212, 4, ENG EK307, 4, ENG ME357, 2, CAS MA226, 4, ENG EC381, 4, ENG EK301, 4, ENG EK210, 2, ENG ME304, 4, ENG ME303, 4, ENG ME305, 4, ENG ME358, 2, ENG ME306, 4, ENG ME419, 4, ENG ME302, 4, ENG ME360, 4, ENG ME310, 4, ENG ME460, 4, ENG ME461, 4");
+
+    list->insertEnd("Electives, Four Advanced Electives");
+
+    list->insertEnd("Hub Credits, PIL, AE, HC, SI, IC, GCI, GCI, ER");
+
+    //Electrical Engineering
+
+    list->insertEnd("Electrical Engineering");
+
+    list->insertEnd("Required, CAS MA123, 4, CAS CH 131, 4, ENG EK100, 0, ENG EK125, 4, CAS WR120, 4, CAS MA124, 4, CAS PY211, 4, ENG EK131, 2, ENG EK103, 3, CAS WR150, 4, CAS MA225, 4, CAS PY212, 4, ENG EK307, 4, ENG EK210, 2, CAS MA226, 4, CAS PY 313, 4, ENG EK301, 4, ENG EC455, 4, ENG EC401, 4, ENG EC311, 4, ENG EC311, 4, ENG EK381, 4, END EC 463, 4, ENG EC 464, 4");
+
+    list->insertEnd("Electives, One Electronics Elective, One Systems Elective, One Electrophysics Elective, One Computer Elective, Three Technical Electives");
+
+    list->insertEnd("Hub Credits, PIL, AE, HC, SI, IC, GCI, GCI, ER");
+
+    //
+
     cout << "Circular doubly linked list & its elements constructed." << endl; 
     
     list->display(list->head);
