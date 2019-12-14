d6427003630fa7befbd851415bafded51e58b9e4
chasem51 <56003396+chasem51@users.noreply.github.com>
2019-12-13 23:25:38 -0500

    Add files via upload

--- src/DataBaseFuncs.cs
+++ src/DataBaseFuncs.cs
@@ -1,17 +1,17 @@
 ﻿#define DATABASEFUNCS
 
 using System;
-using Systems.Collections.Generic;
+using System.Collections.Generic;
 
 public class DataBase
 {
 
     // Driver code 
-    public static void MakeMajors()
+    public static LinkedList<String> MakeMajors()
     {
 
         // Creating a LinkedList of Strings 
-        LinkedList<String> Majors = new LinkedList<String>();
+        var Majors = new LinkedList<String>();
 
         // Biomedical Engineering 
         Majors.AddLast("Biomedical Engineering");
@@ -40,70 +40,338 @@ public class DataBase
         Majors.AddLast("Electives, One Electronics Elective, One Systems Elective, One Electrophysics Elective, One Computer Elective, Three Technical Electives");
         Majors.AddLast("Hub Credits, PIL, AE, HC, SI, IC, GCI, GCI, ER");
 
-        Majors.AddLast(NULL);
+        Majors.AddLast("0");
+
+        return Majors;
     }
 
-    public List<String> ReturnMajor()
-    {
-        string current = Majors.First;
-        List<String> majorlist = new List<String>();
-        string[] majorarray;
 
+
+    static public List<String> ReturnMajor()
+    {
+        LinkedList<String> Majors = MakeMajors();
+        var current = Majors.First;
+        var currentalias = Majors.First.Next;
+        List<String> majorlist = new List<String>();
+        /*
         do
         {
-            if (current == "Mechanical Engineering" || current == "Electrical Engineering" || current == "Computer Engineering" || current == "Biomedical Engineering")
+            if (current.ToString() == "Mechanical Engineering" || current.ToString() == "Electrical Engineering" || current.ToString() == "Computer Engineering" || current.ToString() == "Biomedical Engineering")
             {
-                majorlist.AddLast(current);
+                majorlist.Add(current.ToString());
             }
-            current = current.Next;
-        } while ((current = current.Next) != null);
+            current = currentalias;
+            currentalias = currentalias.Next;
+        } while ((currentalias) != null);
+        */
 
-        majorarray = majorlist.ToArray();
+        majorlist.Add("Mechanical Engineering");
+        majorlist.Add("Elecrical Engineering");
+        majorlist.Add("Computer Engineering");
+        majorlist.Add("Biomedical Engineering");
+
+
         return majorlist;
     }
 
-    public List<String> ReturnMajor_courses(string userinput)
+    static string coursestring;
+    static public List<String> ReturnMajor_courses(string userinput)
     {
-        string current = Majors.First;
+        LinkedList<String> Majors = MakeMajors();
+        var current = Majors.First;
         List<String> majorcourses = new List<String>();
-        string coursestring;
-        do
-        {
-            if (current == userinput)
-            {
-                coursestring = current.Next;
-            }
-        } while ((current = current.Next) != null);
+        /*
+       do
+       {
+           if (current.ToString() == userinput)
+           {
+               coursestring = current.ToString();
+           }
+           current = current.Next;
+       } while ((current) != null);
 
-        string[] coursearrayandcredit = coursestring.split(',');
+       string[] coursearrayandcredit = coursestring.Split(',');
 
-        for (i = 1; i < coursearrayandcredit.size; i + 2)
+
+       for (int i = 1; i < coursearrayandcredit.Length; i = i+2)
+       {
+           majorcourses.Add(coursearrayandcredit[i]);
+           majorcourseelectives.add("One Systems Elective")
+            majorcourseelectives.add("One Electrophysics Elective");
+            majorcourseelectives.add("One Computer Elective");
+            majorcourseelectives.add("Three Technical Electives");
+       }
+       */
+
+        if (userinput == "Biomedical Engineering")
         {
-            majorcourses.AddLast(coursearrayandcredit[i]);
+            majorcourses.add("CAS MA123");
+            majorcourses.add("ENG EK100");
+            majorcourses.add("CAS CH101");
+            majorcourses.add("ENG EK125");
+            majorcourses.add("CAS WR120");
+            majorcourses.add("CAS MA124");
+            majorcourses.add("CAS PY211");
+            majorcourses.add("CAS CH102");
+            majorcourses.add("ENG EK131");
+            majorcourses.add("ENG EK103");
+            majorcourses.add("CAS MA225");
+            majorcourses.add("CAS PY212");
+            majorcourses.add("ENG EK307");
+            majorcourses.add("ENG EK210");
+            majorcourses.add("CAS WR150");
+            majorcourses.add("CAS MA226");
+            majorcourses.add("ENG BE209");
+            majorcourses.add("ENG EK301");
+            majorcourses.add("ENG EK381");
+            majorcourses.add("CAS BI315");
+            majorcourses.add("ENG BE403");
+            majorcourses.add("ENG BE403");
+            majorcourses.add("ENG BE491");
+            majorcourses.add("ENG EK424");
+            majorcourses.add("ENG BE492");
+            majorcourses.add("ENG BE465");
+            majorcourses.add("ENG BE466");
+        }
+        else if (userinput == "Computer Engineering")
+        {
+            majorcourses.add("CAS MA123");
+            majorcourses.add("CAS CH131");
+            majorcourses.add("ENG EK100");
+            majorcourses.add("ENG EK125");
+            majorcourses.add("CAS WR120");
+            majorcourses.add("CAS MA124");
+            majorcourses.add("CAS PY211");
+            majorcourses.add("ENG EK131");
+            majorcourses.add("ENG EK103");
+            majorcourses.add("CAS WR150");
+            majorcourses.add("CAS MA225");
+            majorcourses.add("CAS PY212");
+            majorcourses.add("ENG EK307");
+            majorcourses.add("ENG EK327");
+            majorcourses.add("CAS MA226");
+            majorcourses.add("ENG EC311");
+            majorcourses.add("ENG EK301");
+            majorcourses.add("ENG EK210");
+            majorcourses.add("ENG EC330");
+            majorcourses.add("ENG EK381");
+            majorcourses.add("ENG EC413");
+            majorcourses.add("CAS MA193");
+            majorcourses.add("ENG EC463");
+            majorcourses.add("ENG EC464");
+
+
+        }
+        else if (userinput == "Mechanical Engineering")
+        {
+            majorcourses.add("CAS MA123");
+            majorcourses.add("CAS CH131");
+            majorcourses.add("ENG EK100");
+            majorcourses.add("ENG EK125");
+            majorcourses.add("CAS WR120");
+            majorcourses.add("CAS MA124");
+            majorcourses.add("CAS PY211");
+            majorcourses.add("ENG EK131");
+            majorcourses.add("ENG EK103");
+            majorcourses.add("CAS WR150");
+            majorcourses.add("CAS MA225");
+            majorcourses.add("CAS PY212");
+            majorcourses.add("ENG EK307");
+            majorcourses.add("ENG ME357");
+            majorcourses.add("CAS MA226");
+            majorcourses.add("ENG EC381");
+            majorcourses.add("ENG EK301");
+            majorcourses.add("ENG EK210");
+            majorcourses.add("ENG ME304");
+            majorcourses.add("ENG ME303");
+            majorcourses.add("ENG ME305");
+            majorcourses.add("ENG ME358");
+            majorcourses.add("ENG ME306");
+            majorcourses.add("ENG ME419");
+            majorcourses.add("ENG ME302");
+            majorcourses.add("ENG ME360");
+            majorcourses.add("ENG ME310");
+            majorcourses.add("ENG ME460");
+            majorcourses.add("ENG ME461");
+
+        }
+        else if (userinput == "Electrical Engineering")
+        {
+            majorcourses.Add("CAS MA123");
+            majorcourses.Add("CAS CH131");
+            majorcourses.Add("ENG EK100");
+            majorcourses.Add("ENG EK125");
+            majorcourses.Add("CAS WR120");
+            majorcourses.Add("CAS MA124");
+            majorcourses.Add("CAS PY211");
+            majorcourses.Add("ENG EK131");
+            majorcourses.Add("ENG EK103");
+            majorcourses.Add("CAS WR150");
+            majorcourses.Add("CAS MA225");
+            majorcourses.Add("CAS PY212");
+            majorcourses.Add("ENG EK307");
+            majorcourses.Add("ENG EK210");
+            majorcourses.Add("CAS MA226");
+            majorcourses.Add("CAS PY313");
+            majorcourses.Add("ENG EK301");
+            majorcourses.Add("ENG EC455");
+            majorcourses.Add("ENG EC401");
+            majorcourses.Add("ENG EC311");
+            majorcourses.Add("ENG EK381");
+            majorcourses.Add("ENG EC464");
         }
 
         return majorcourses;
 
+
     }
 
-    public List<String> ReturnMajor_coursecredits(string userinput)
+    static public List<String> ReturnMajor_coursecredits(string userinput)
     {
-        string current = Majors.First;
+        LinkedList<String> Majors = MakeMajors();
+        var current = Majors.First;
         List<String> majorcoursecredits = new List<String>();
-        string coursestring;
+
+        /*
         do
         {
-            if (current == userinput)
+            if (current.ToString() == userinput)
             {
-                coursestring = current.Next;
+                coursestring = Majors.First.Next.ToString();
             }
-        } while ((current = current.Next) != null);
+            current = current.Next;
+        } while ((current.ToString()) != "0");
 
-        string[] coursearrayandcredit = coursestring.split(',');
+        string[] coursearrayandcredit = coursestring.Split(',');
 
-        for (i = 2; i < coursearrayandcredit.size; i + 2)
+        for (int i = 2; i < coursearrayandcredit.Length; i = i + 2)
         {
-            majorcoursecredits.AddLast(coursearrayandcredit[i]);
+            majorcoursecredits.Add(coursearrayandcredit[i]);
+        }
+        */
+
+        if (userinput == "Biomedical Engineering")
+        {
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("0");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("3");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+        }
+        else if (userinput == "Computer Engineering")
+        {
+
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("0");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("3");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+        }
+        else if (userinput == "Mechanical Engineering")
+        {
+
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("0");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("3");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+        }
+        else if (userinput == "Electrical Engineering")
+        {
+
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("0");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("3");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("4");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("2");
+            majorcoursecredits.add("4");
         }
 
         return majorcoursecredits;
@@ -111,54 +379,100 @@ public class DataBase
     }
 
 
-
-    public List<String> ReturnMajor_electives(string userinput)
+    static string electivestring;
+    static public List<String> ReturnMajor_electives(string userinput)
     {
-        string current = Majors.First;
+        LinkedList<String> Majors = MakeMajors();
+        var current = Majors.First;
         List<String> majorcourseelectives = new List<String>();
-        string electivestring;
+
+        /*
         do
         {
-            if (current == userinput)
+            if (current.ToString() == userinput)
             {
-                current = current.Next;
-                electivestring = current.Next;
+                current =current.Next;
+                electivestring = current.Next.ToString();
             }
-        } while ((current = current.Next) != null);
+            current = current.Next;
+        } while ((current) != null);
 
-        string[] electives = electivestring.split(',');
+        string[] electives = electivestring.Split(',');
 
-        for (i = 0; i < electives.size; i++)
+        for (int i = 0; i < electives.Length; i++)
         {
-            majorcourseelectives.AddLast(electives[i]);
+            majorcourseelectives.Add(electives[i]);
         }
+        */
+        if (userinput == "Biomedical Engineering")
+        {
+            majorcourseelectives.add("One Continua & Fields Elective");
+            majorcourseelectives.add("Two Suitable Professional Electives");
+            majorcourseelectives.add("One Engineering elective");
+            majorcourseelectives.add("Two Biomedical engineering electives");
+            majorcourseelectives.add("One Biomedical engineering design elective");
+        }
+        else if (userinput == "Computer Engineering")
+        {
+            majorcourseelectives.add("Two Core Electives");
+            majorcourseelectives.add("Two Computer Engineering Electives");
+            majorcourseelectives.add("One EE Breadth Elective");
+            majorcourseelectives.add("Technical Electives");
+        }
+        else if (userinput == "Mechanical Engineering")
+        {
+            majorcourseelectives.add("Four Advanced Electives");
+        }
+        else if (userinput == "Electrical Engineering")
+        {
+            majorcourseelectives.add("One Systems Elective");
+            majorcourseelectives.add("One Electrophysics Elective");
+            majorcourseelectives.add("One Computer Elective");
+            majorcourseelectives.add("Three Technical Electives");
+        }
+
 
         return majorcourseelectives;
     }
 
 
-
-    public List<String> ReturnMajor_hub(string userinput)
+    static string hubstring;
+    static public List<String> ReturnMajor_hub(string userinput)
     {
-        string current = Majors.First;
+        LinkedList<String> Majors = MakeMajors();
+        var current = Majors.First;
         List<String> majorcoursehubs = new List<String>();
-        string hubstring;
+
+        /*
         do
         {
-            if (current == userinput)
+            if (current.ToString() == userinput)
             {
                 current = current.Next;
                 current = current.Next;
-                hubstring = current.Next;
+                hubstring = current.Next.ToString();
             }
+
+            current = current.Next;
         } while ((current = current.Next) != null);
 
-        string[] hubs = hubstring.split(',');
+        string[] hubs = hubstring.Split(',');
 
-        for (i = 0; i < hubs.size; i++)
+        for (int i = 0; i < hubs.Length; i++)
         {
-            majorcoursehubs.AddLast(electives[i]);
+            majorcoursehubs.Add(hubs[i]);
         }
+        */
+        majorcoursehubs.add("PIL");
+        majorcoursehubs.add("AE");
+        majorcoursehubs.add("HC");
+        majorcoursehubs.add("SI");
+        majorcoursehubs.add("IC");
+        majorcoursehubs.add("GCI");
+        majorcoursehubs.add("GCI");
+        majorcoursehubs.add("ER");
+
+
 
         return majorcoursehubs;
     }
